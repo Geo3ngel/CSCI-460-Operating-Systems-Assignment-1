0afb6e85dffc2db5e24a7b9ca4b422dd026c4e5d
GeoEngel <GeoEngel.z@gmail.com>
2019-10-14 10:30:47 -0600

    Added Arguments to main w/ default values.

--- src/Main.c
+++ src/Main.c
@@ -4,20 +4,41 @@
 #include <stdlib.h>
 
 // Constants:
-#define MAXCARS 3
-
 #define TO_BRIDGER 0
 #define TO_BOZEMAN 1
 
 
 // Global varibales:
 
-// Main Program Loop
+_Atomic int* Cars_On_OneWay;
 
+// Main Program Loop
+int main(int argc, char* argv[]){
+    // Initializes iterator
+    int iter = 0;
+    int thread_count;
+    int max_cars;
+
+    if( argc < 2){
+        // Set up values to a feault configuration rather than specified thread count/car max.
+        thread_count = 10;
+        max_cars = 3;
+    } else{
+        // Initializes the maximum about of threads allowed.
+        thread_count = atoi(argv[1]);
+
+        // initializes the maximum threshold of cars the one way can handle at a time.
+        max_cars = atoi(argv[2]);
+    }
+    
+        printf("Initializing with %d threads.\n", thread_count);
+
+        printf("%d is the maximum amount of cars allowed on the one-way.\n", max_cars);
+}
 // Vehicle simulation
 
 // NOTE: represent each vehicle as a thread.
-OneVehicle(direction){
+void* OneVehicle(void* direction){
     ArriveBridgerOneWay(direction);
     //now the car is on the one-way section!
 
@@ -28,7 +49,7 @@ OneVehicle(direction){
 }
 
 // Puts a car on the one-way once it is confirmed to be safe.
-void* ArriveBridgerOneWay(void* direction){
+int ArriveBridgerOneWay(void* direction){
 // TODO: Must not return until its safe for the car to get on the one-way
 // Checks if it is safe for the car to go on the one way
 
@@ -37,6 +58,18 @@ void* ArriveBridgerOneWay(void* direction){
 }
 
 // Outputs the car's state as it passes through the one way.
-void* OnBridgerOneWay(void* direction){
+int OnBridgerOneWay(void* direction){
 // TODO: Use prints or graphics library?
+
+// Checks the capacity of the one way
+
+// Monitors for race conditions via visual output!
+}
+
+// Removes the car from the one way
+int ExitBridgerOneWay(void* direction){
+// Removes car from critical area?
+
+// Unlocks access to this area?
+
 }
